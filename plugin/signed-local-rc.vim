5baceeb309f2fdc53106814940df60b304de6a86
Nick Spinale <spinalen@carleton.edu>
2017-01-12 01:48:24 -0500

    add actual commands

--- plugin/signed-local-rc.vim
+++ plugin/signed-local-rc.vim
@@ -13,7 +13,12 @@ import sys, os.path
 this = vim.eval('expand("<sfile>")')
 lib = os.path.join(os.path.dirname(this), '..', 'python')
 sys.path.append(lib)
-import slrc.vimsupport
+import slrc.vimsupport as slrc
 EOF
 
 let g:signed_local_rc_trusted_dir = '~/.signed_local_rc_trusted_dir'
+
+command! SlrcSource py slrc.checked_source()
+command! -nargs=1 SlrcTrust py slrc.trust_pub_key(<f-args>)
+command! -nargs=1 SlrcUntrust py slrc.untrust_pub_key(<f-args>)
+command! -nargs=1 SlrcSign py slrc.sign_vimrc(<f-args>)
--- python/slrc/vimsupport.py
+++ python/slrc/vimsupport.py
@@ -1,14 +1,25 @@
 import os.path
 
 from slrc.crypto import sign_file, verify_file
-from slrc.persist import check_pub_key, trust_pub_key
+from slrc.persist import check_pub_key, trust_pub_key, untrust_pub_key
 
 import vim
 
 
-if (
-    os.path.isfile('.vimrc.local')
-    and os.path.isfile('.vimrc.local.pub')
-    and os.path.isfile('.vimrc.local.sig')
-    ):
-    print 'hi'
+def checked_source():
+    if (
+        os.path.isfile('.vimrc')
+        and os.path.isfile('.vimrc.pub')
+        and os.path.isfile('.vimrc.sig')
+        ):
+        if check_pub_key('.vimrc.pub') and verify_file('.vimrc.pub', '.vimrc', '.vimrc.sig'):
+            vim.command('source .vimrc')
+        else:
+            vim.command('echo "Not loading untrusted vimrc"')
+
+
+def sign_vimrc(priv_key_path):
+    if os.path.isfile('.vimrc'):
+        sign_file(priv_key_path, '.vimrc', '.vimrc.sig')
+    else:
+        vim.command('echo ".vimrc not found in cwd"')
